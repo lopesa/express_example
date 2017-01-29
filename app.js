3aa22b9a2d7144075cc9dbaf99980a55e5c3e863
Tony Lopes <tony@lopesdesign.com>
2017-01-29 11:39:14 -0800

    fixes breaking change and changes head again

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // catch 404 and forward to error handler
-app.us(function(req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
